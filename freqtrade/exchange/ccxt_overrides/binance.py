3c8a3fd20b7b37c2bbbb8976940de3443c8c2496
Vlad R <vlad180597@gmail.com>
2024-04-25 14:04:32 +0000

    add data sample and ranames

--- freqtrade/exchange/ccxt_overrides/binance.py
+++ freqtrade/exchange/ccxt_overrides/binance.py
@@ -7,7 +7,7 @@ class BinanceOverrideAsync(async_binance):
         inverse = self.safe_bool(market, 'inverse')
         volumeIndex = 7 if inverse else 5
         configured_df_cols = Configuration.get_static_config()["dataframe_columns"]
-        columns_to_index_map = {"openTime":0,
+        number_columns_to_index = {"openTime":0,
                                 "open":1,
                                 "high":2,
                                 "low":3,
@@ -17,13 +17,13 @@ class BinanceOverrideAsync(async_binance):
                                 "taker_buy_volume":10
                                 }
         open_time_col = self.safe_integer_2(ohlcv, 0, 'openTime')
-        return [open_time_col]+[self.safe_number_2(ohlcv,x,columns_to_index_map[x])  for x in configured_df_cols if x in columns_to_index_map]
+        return [open_time_col]+[self.safe_number_2(ohlcv,x,number_columns_to_index[x])  for x in configured_df_cols if x in number_columns_to_index]
 class BinanceOverride(ccxt_binance):
     def parse_ohlcv(self, ohlcv, market=None):
         inverse = self.safe_bool(market, 'inverse')
         volumeIndex = 7 if inverse else 5
         configured_df_cols = Configuration.get_static_config()["dataframe_columns"]
-        columns_to_index_map = {"openTime":0,
+        number_columns_to_index = {"openTime":0,
                                 "open":1,
                                 "high":2,
                                 "low":3,
@@ -33,4 +33,4 @@ class BinanceOverride(ccxt_binance):
                                 "taker_buy_volume":10
                                 }
         open_time_col = self.safe_integer_2(ohlcv, 0, 'openTime')
-        return [open_time_col]+[self.safe_number_2(ohlcv,x,columns_to_index_map[x])  for x in configured_df_cols if x in columns_to_index_map]
+        return [open_time_col]+[self.safe_number_2(ohlcv,x,number_columns_to_index[x])  for x in configured_df_cols if x in number_columns_to_index]
